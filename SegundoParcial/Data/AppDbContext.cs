af9bf8ee7fc44363df1528b174382984989834ac
Roberto <a2020113431@estudiantes.upsa.edu.bo>
2021-10-13 19:09:43 -0400

    Agregar archivos de proyecto.

---
+++ SegundoParcial/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SegundoParcial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        // Agregar los modelos a utilizar
+        public DbSet<Suerte> Suerte { get; set; } //modelo cancion en este caso
+    }
+}
---
+++ SegundoParcial/Models/Suerte.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcial.Models
+{
+    public class Suerte
+    {
+        [Key] //llave primaria 
+        public int SuerteId { get; set; } 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "{0} es requerido")] //data anotation todos los campos son requeridos, no se permiten campos vacios o muestra mensaje de error
+        public string Detalle { get; set; } //
+        [Url]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "La longitud de {0} debe estar entre {2} y {1}")]
+        public string Imagen { get; set; }
+    }
+}
